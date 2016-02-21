a2d111377e4e262e9de9c480998ea5f746daed7f
pp9 <pittbrad63@mail.ru>
2016-02-22 01:37:13 +0300

    init commit

---
+++ js/mquery.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function nodeListToArr(list) {
+    var arr = [];
+    for (var i = 0; i < list.length; i++) {
+        arr[i] = list[i];
+    }
+    return arr;
+}
+
+function mainSelect(sel) {
+    var selectResult = document.querySelectorAll(sel);
+    var arr = nodeListToArr(selectResult);
+    var resp = {};
+    resp.nodes = arr;
+    resp.length = selectResult.length;
+    return resp;
+}
+
+function makeQuery(selector, context) {
+    if(selector) {
+        var sel;
+        // build selector
+        if(context) {
+            if(context instanceof mQuery) {
+                sel = context.selector + ' ' + selector;
+            } else {
+                sel = context + ' ' + selector;
+            }
+        } else {
+            sel = selector;
+        }
+        var rsp = {};
+        var selNodes = mainSelect(sel);
+
+        rsp.selected = selNodes.nodes;
+        rsp.length = selNodes.length;
+        rsp.selector = sel;
+        return rsp;
+    }
+}
+
+function mQuery() {};
+
+mQuery.prototype.add = function (selector, context) {
+    var rsp = makeQuery(selector, context);
+
+    for (var i = 0, b = this.length; i < rsp.length; i++, b++) {
+        this[b] = rsp.selected[i];
+    }
+    delete this.selector;
+    return this;
+};
+
+
+var m$ = function(selector, context) {
+
+    var mainObj = new mQuery();
+
+    var rsp = makeQuery(selector, context);
+
+    for (var i = 0; i < rsp.selected.length; i++) {
+        mainObj[i] = rsp.selected[i];
+    }
+    mainObj.length = rsp.length;
+    mainObj.selector = rsp.selector;
+
+    return mainObj;
+}
+
+
+// var ctx = m$('.wrapp');
+var m$res = m$('li').add('.wrapp');
+var $res = $('li', 'ul').add('.wrapp');
+
+// console.log(ctx);
+console.log(m$res);
+console.log($res);
